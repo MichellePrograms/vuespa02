8aca737ba5e3cf29d1f8ea636c37a4700a565ebd
MichellePrograms <it@petersonprograms.info>
2018-03-18 12:03:39 -0400

    webpack dev middle ware module behaving

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 const app = new Vue({
   data: {
-    hello: 'hup hup hup'
+    hello: 'working on two'
   }
   //dont use template yet cuz it is just fillingin the hello on the index.html page right now!!
   // breaks it
